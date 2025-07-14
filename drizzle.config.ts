514463ab9ed6207abd262b0d121a3a496c93f19a
amutake <amutake.s@gmail.com>
2025-07-14 13:15:00 +0900

    migration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
   dialect: 'sqlite',
-  schema: './app/db/schema.ts',
+  schema: './app/repositories/schema.ts',
   casing: 'snake_case',
   out: './migrations',
 });
