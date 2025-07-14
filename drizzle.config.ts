f6ea0ef129c1fd6065a5c77c68bfa8e406228472
amutake <amutake.s@gmail.com>
2025-07-14 13:01:03 +0900

    biome format

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,8 +1,8 @@
-import { defineConfig } from "drizzle-kit";
+import { defineConfig } from 'drizzle-kit';
 
 export default defineConfig({
-  dialect: "sqlite",
-  schema: "./app/db/schema.ts",
-  casing: "snake_case",
-  out: "./migrations",
+  dialect: 'sqlite',
+  schema: './app/db/schema.ts',
+  casing: 'snake_case',
+  out: './migrations',
 });
